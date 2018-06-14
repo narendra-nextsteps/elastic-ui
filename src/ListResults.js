eaf0ee4f3fb41d3e0647185a9daae4653052e44b
Tummala Dharma Teja <dharmateja.tummala@nextsteps.ai>
2018-06-14 08:57:24 +0530

    styling

--- src/ListResults.js
+++ src/ListResults.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactHtmlParser from 'react-html-parser';
 
 const contentStyle = {
-    backgroundColor:'gray',
+    backgroundColor:'#e5e5e5',
     fontSize:'1rem',
     padding: '1% 2%',
     margin: '5px',
@@ -14,13 +14,11 @@ const ListResults = (props) => {
         <div style={{padding:'2px 10px', margin:'1% 10%'}}>
             {
                 props.searchResult.map((value, index) => (
-                    <div>
                     <div key={index} style={contentStyle}>
                         {
                             ReactHtmlParser(value._source.ParameterValue)
                         }
                         {/* <div style={{color:'blue'}}>{value._source.ParameterValueForSearch}</div> */}
-                    </div>
                     </div>
                 ))
             }
